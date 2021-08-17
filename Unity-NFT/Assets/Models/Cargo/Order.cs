fadf8ca9c01267b8779914415d4ba76e0794956d
faha-berdiev <62469844+faha-berdiev@users.noreply.github.com>
2021-08-17 23:54:56 +0500

    added order model

---
+++ Unity-NFT/Assets/Models/Cargo/Order.cs
@@ -0,0 +1,38 @@
+﻿namespace Models.Cargo
+{
+    /*
+     * export type OrderParams = {
+  contractAddress?: string;
+  vendorId?: string;
+  sellerAddress?: string;
+  buyerAddress?: string;
+  tokenId?: string;
+  crateId?: string;
+  from?: string;
+  to?: string;
+  limit?: string;
+  page?: string;
+};
+
+export type Order = {
+  seller: string;
+  buyer: string;
+  uuid: string;
+  contract: string;
+  tokenId: string;
+  price: string;
+  fees: {
+    cargo: string;
+    crate: string;
+  };
+  beneficiaries: { address: string; commission: string }[];
+  vendorId: string;
+  crateId: string;
+  createdAt: string;
+};
+     */
+    public class Order
+    { 
+        public string Seller { get; set; }
+    }
+}
