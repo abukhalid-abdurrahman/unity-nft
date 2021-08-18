a5993c1e46bf0dd4516d6f157748c24ff2ee0de2
faha-berdiev <62469844+faha-berdiev@users.noreply.github.com>
2021-08-18 16:26:13 +0500

    PaginationResponseWithResults added

---
+++ Unity-NFT/Assets/Models/Cargo/PaginationResponseWithResults.cs
@@ -0,0 +1,17 @@
+﻿namespace Models.Cargo
+{
+    /*export type PaginationResponseWithResults<R> = {
+  page: string;
+  totalPages: string;
+  limit: string;
+  results: R;
+};
+*/
+    public class PaginationResponseWithResults<T>
+    {
+        public string Page { get; set; }
+        public string TotalPage { get; set; }
+        public string Limit { get; set; }
+        public T Result { get; set; }
+    }
+}
